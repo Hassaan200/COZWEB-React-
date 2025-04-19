96f3e5baf40e65de3b168605353d63598aaf3426
Hassaan200 <hassaankhaliq45@gmail.com>
2025-04-19 15:02:40 +0500

    fix Responsivness

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -26,11 +26,11 @@ const Footer = () => {
             <br />
             <div className="flex items-center gap-3">
               <FaLocationArrow />
-              <p>Noida, Uttar Pradesh</p>
+              <p>Karachi, Pakistan</p>
             </div>
             <div className="mt-3 flex items-center gap-3">
               <FaMobileAlt />
-              <p>+91 123456789</p>
+              <p>+92 335 95540 95</p>
             </div>
             {/* Social Handle */}
           </div>
@@ -104,7 +104,7 @@ const Footer = () => {
         </div>
         <div>
           <div className="border-t-2 border-gray-300/50 py-6 text-center">
-            @copyright 2024 Travery || Dilshad
+            @copyright 2025 All rights reserved
           </div>
         </div>
       </section>
